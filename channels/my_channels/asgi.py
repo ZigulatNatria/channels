ae24dfe2b8a244cf593ffa2d0cc5338d7c3cf91f
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-11-20 02:06:34 +0500

    initial

---
+++ channels/my_channels/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for my_channels project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+from channels.routing import ProtocolTypeRouter
+
+
+django_asgi_app = get_asgi_application()
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_channels.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+})
