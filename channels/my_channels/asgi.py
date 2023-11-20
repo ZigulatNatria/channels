879819ab7e68953872c41ad99022bd94dc18d7b0
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-11-21 00:42:47 +0500

    Подключен консумер и роутинг

--- channels/my_channels/asgi.py
+++ channels/my_channels/asgi.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 
+from chat.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 
@@ -24,5 +27,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_channels.settings')
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     # Just HTTP for now. (We can add other protocols later.)
 })
